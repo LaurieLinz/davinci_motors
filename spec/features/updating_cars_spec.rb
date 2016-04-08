cf1eb329a13aafa4844a4c450c543600c3517724
Laurie Linz <lvlinz@comcast.net>
2016-04-08 16:15:37 -0600

    Removed unneeded code
    [#115990861]

--- spec/features/updating_cars_spec.rb
+++ spec/features/updating_cars_spec.rb
@@ -8,15 +8,6 @@ require 'rails_helper'
       car.save
     visit '/'
 
-    # click_link 'New Car'
-    #
-    # fill_in 'Make', with: 'Ford'
-    # fill_in 'Model', with: 'Mustang'
-    # fill_in 'Year', with: '1967'
-    # fill_in 'Price', with: '2300'
-    #
-    # click_button 'Create Car'
-    # expect(page).to have_content('1967 Ford Mustang created')
       click_link 'Edit'
 
       fill_in 'Year', with:'1968'
